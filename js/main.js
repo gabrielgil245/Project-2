d94d7bf231b965a9d8a7baf566b42a76304f6ee5
gabrielgil245 <gabrielgil245@gmail.com>
2020-11-11 21:55:56 -0600

    Revisited API functionality, enabling users to search GIFs based on phrases, while limiting the number of GIFs they wish to search for, and replaced the No. of GIFs dropdown menu with a search field.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
 $(document).ready(function(){
     console.log("Ready!");
 
-    $("#get-from-api").on("click",async() => {
-        console.log("Search button was clicked!");
-        //"NOTE: keyword = "nintendo" is a placeholder example
-        //"NOTE: limit of 10 GIFs is a placeholder example too"
-        fetch(`https://api.giphy.com/v1/gifs/search?api_key=i2WLb8zCXTk3RPER0hdoUV2OFWCzY3nf&q=nintendo&limit=10`)
+    $("#get-from-api").on("click", async() => {       
+        let searchTerm = $("#search-field").val();
+        let limitValue = $("#GIF-field").val();
+        // console.log($("#search-field").val());
+        // console.log($("#GIF-field").val());
+
+        let url = "https://api.giphy.com/v1/gifs/search?api_key=i2WLb8zCXTk3RPER0hdoUV2OFWCzY3nf&q=" + searchTerm + "&limit=" + limitValue;
+        
+        fetch(url)
         .then((results) => {
             return results.json();
         })
         .then((giphyResponse) => {
+
+            let resultsHTML = "";
+
             for (let image of giphyResponse.data)
             {
-                let imgHtml = `<img src=${image.images.original.url}/>`
-                $("#search-results").append(imgHtml);
+                let imgHTML = `<img class="col-12 col-lg-4" src=${image.images.original.url}/>`;
+                resultsHTML = resultsHTML + imgHTML;
+                $("#search-results").html(resultsHTML);
             }
         })
     })
